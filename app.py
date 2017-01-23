9f0ebcd653b9c36d1b80a71e488ff2255c9f34ed
Athenais Vaginay <vaginay.athenais@gmail.com>
2017-01-23 14:24:05 +0100

    Add log views and rendering (signup, login, logout, account)

--- app.py
+++ app.py
@@ -12,6 +12,30 @@ def main():
     """
     return render_template('index.html')
 
+@app.route("/signup")
+def signup():
+    """
+    """
+    return render_template('signup.html')
+
+@app.route("/login")
+def login():
+    """
+    """
+    return render_template('login.html')
+
+@app.route("/logout")
+def logout():
+    """
+    """
+    return "logout"
+
+@app.route("/account")
+def account():
+    """
+    """
+    return render_template('account-page.html')
+
 
 if __name__ == "__main__":
     # Run the app if the executed file is the main program
