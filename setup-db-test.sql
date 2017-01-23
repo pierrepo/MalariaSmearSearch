058962a855f275b10e447274119032796359bb17
Athenais Vaginay <vaginay.athenais@gmail.com>
2017-01-23 17:34:45 +0100

    Basic user table + 2 inserts. Used for tests.
    (We avoid as much as possible AUTOINCREMENT keyword that imposes extra CPU, memory, disk space, and disk I/O overhead).

---
+++ setup-db-test.sql
@@ -0,0 +1,26 @@
+
+CREATE TABLE tbl_level (
+  'name' VARCHAR(45) PRIMARY KEY
+);
+
+INSERT INTO tbl_level(level_name) VALUES ('technician'); 
+INSERT INTO tbl_level(level_name) VALUES ('supervisor');
+
+CREATE TABLE tbl_user (
+
+  'username' VARCHAR(45) PRIMARY KEY ,
+  'email' VARCHAR(45) NULL,
+  'password' VARCHAR(45) NULL,
+  'level' REFERENCES tbl_level(name), 
+  'institution'  VARCHAR(45) NULL
+);
+  
+
+INSERT INTO tbl_user VALUES ('imtech', 'tech@test.com',  'pswtech', 'technician', 'up7d') ;
+INSERT INTO tbl_user VALUES  ( 'imsup','sup@sup.com', 'pswsup', 'supervisor', 'up7d');
+
+
+SELECT * FROM tbl_user ;
+
+.quit
+
