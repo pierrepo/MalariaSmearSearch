9f9030911ffc26e2b601fffe1108207f0a5da8c3
Athenais Vaginay <vaginay.athenais@gmail.com>
2017-01-24 16:12:33 +0100

    User class is now usable by Flask-Login extention

--- model.py
+++ model.py
@@ -1,12 +1,13 @@
 from app import app
 from flask_sqlalchemy import SQLAlchemy
+from flask_login import UserMixin
 
 db = SQLAlchemy(app)
 
 # Table already exist, so do not redefine it and just
 # load them from the database using the "autoload" feature.
 
-class User(db.Model):
+class User(db.Model, UserMixin):
     __tablename__ = 'tbl_user'
     __table_args__ = {
         'autoload': True,
@@ -22,3 +23,15 @@ class User(db.Model):
             self.level,
             self.institution
         )
+
+    #UserMixin inheritance provide basic implementation for
+    #   is_authenticated
+    #   is_active
+    #   is_anonymous
+    # properties and
+    #   get_id() method
+
+    # TODO : get_id use id attribut -> replace username attr by id
+    @property
+    def id (self) :
+        return self.username
