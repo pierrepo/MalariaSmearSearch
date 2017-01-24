33cbf90554c596e4e26a0d4d5114b70959618205
Athenais Vaginay <vaginay.athenais@gmail.com>
2017-01-24 16:09:01 +0100

    Configure the app with the Flask-Login extention

--- app.py
+++ app.py
@@ -1,7 +1,12 @@
 from flask import Flask
+from flask_login import LoginManager
 
 import config
 
 app = Flask(__name__)
 app.config.from_object('config.DevelopmentConfig')
+
+
+login_manager = LoginManager()
+login_manager.init_app(app)
 
