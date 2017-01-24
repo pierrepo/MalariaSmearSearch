734886945dc52b95f0de7c69aed09a61de7779df
Athenais Vaginay <vaginay.athenais@gmail.com>
2017-01-24 13:42:31 +0100

    Move views in separated files

--- app.py
+++ app.py
@@ -1,77 +1,7 @@
-from flask import Flask, render_template
-
+from flask import Flask
 
 import config
 
-
-from forms import RegisterForm
-from model import *
-
 app = Flask(__name__)
 app.config.from_object('config.DevelopmentConfig')
-
-
-@app.route("/")
-def index():
-    """
-    Define the basic route / and its corresponding request handler
-    """
-    return render_template('index.html')
-
-@app.route("/signup", methods = ['GET', 'POST'])
-def signup():
-    """
-    """
-    form = RegisterForm()
-    if form.validate_on_submit() :
-        okay = True
-        #-----
-        # TODO : check provided data in request.form['tag'], flash (u 'msg', 'error') if pb :
-
-        # username not already use,
-
-        # password is secure enough
-
-        # email adress okay
-
-        if okay :
-            # TODO : send confirmation email
-
-            # save the guy
-            new_user = User()
-            form.populate_obj(new_user)
-            print (new_user)
-            print (form)
-
-
-            try :
-                db.session.add(new_user)
-                db.session.commit()
-                print('New user added to database')
-            except Exception as e:
-                print (e)
-                db.session.rollback()
-                print('An error occurred accessing the database.')
-                redirect('/')
-
-    return render_template('signup.html', form=form)
-
-@app.route("/login")
-def login():
-    """
-    """
-    return render_template('login.html')
-
-@app.route("/logout")
-def logout():
-    """
-    """
-    return "logout"
-
-@app.route("/account")
-def account():
-    """
-    """
-    return render_template('account-page.html')
-
 
