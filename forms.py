78d8b20d56cf775ba4fe68010c626b902555dbf9
Athenais Vaginay <vaginay.athenais@gmail.com>
2017-01-24 20:16:23 +0100

    Flask WTF form to upload image & metadata

--- forms.py
+++ forms.py
@@ -1,7 +1,26 @@
 from flask_wtf import FlaskForm
-from wtforms.fields import TextField, BooleanField, PasswordField, RadioField, SubmitField, TextField
+from wtforms.fields import TextField, BooleanField, PasswordField, RadioField, SubmitField, TextField, IntegerField
 from wtforms.validators import Optional,  Required, Email, EqualTo, Length
-    
+from flask_uploads import UploadSet, IMAGES
+from flask_wtf.file import FileField, FileAllowed, FileRequired
+
+import app
+
+class UploadForm(FlaskForm):
+    photo = FileField('photo', validators=[
+        FileRequired(),
+        FileAllowed( app.photos, 'Images only!')
+    ])
+
+    preparation_type = RadioField('Type of preparation', choices=[
+            ('thick',  'Goutte Epaisse '),
+            ('thin',  'Frotti'),
+            ], validators=[Required()] )
+    comment = TextField('Comment', validators=[ Length( max=256)])
+    magnification =IntegerField ('magnification', validators=[Required()])
+    microscope_model = TextField ('microscope_model',  validators=[Required()])
+
+    submit = SubmitField('Upload image')
 
 
 class LoginForm(FlaskForm):
@@ -23,13 +42,13 @@ class ProfileForm(FlaskForm):
             EqualTo('confirm', message='Passwords must match')
             ])
     confirm = PasswordField('Repeat Password')
-    
+
     submit = SubmitField('Update Profile')
 
 
 class RegisterForm(FlaskForm):
     username = TextField('Username', validators=[Required(), Length(min=8, max=80)])
-    
+
     email = TextField('Email Address', validators = [Required(), Email()])
     password = PasswordField('New Password', validators=[
             Required(),
@@ -38,5 +57,5 @@ class RegisterForm(FlaskForm):
     ])
     confirm = PasswordField('Repeat Password')
     accept_tos = BooleanField('I accept the TOS', validators = [Required()])
-    
+
     submit = SubmitField('Register')
