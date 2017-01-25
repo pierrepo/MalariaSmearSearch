bf65389a9e05e01fe5dd7d8b24b464361c342032
Athenais Vaginay <vaginay.athenais@gmail.com>
2017-01-25 14:03:24 +0100

    Add photo table

--- setup-db-test.sql
+++ setup-db-test.sql
@@ -20,6 +20,20 @@ INSERT INTO tbl_user VALUES ('imtech', 'tech@test.com',  'pswtech', 'technician'
 INSERT INTO tbl_user VALUES  ( 'imsup','sup@sup.com', 'pswsup', 'supervisor', 'up7d');
 
 
+CREATE TABLE tbl_photo (
+  'id' INTEGER PRIMARY KEY AUTOINCREMENT ,
+  'username' REFERENCES tbl_user(username),
+  preparation_type VARCHAR(5) CHECK (preparation_type IN ('thick' , 'thin') ),
+  'comment' VARCHAR(256) NULL,
+  'magnification' INTEGER,
+  'microscope_model'  VARCHAR(45)
+);
+
+INSERT INTO tbl_photo ('username', 'preparation_type', 'comment', 'magnification', 'microscope_model')
+    VALUES ('imtech', 'thick',  'je suis un commentaire', 1000, 'super microscope trop swag') ;
+
+
+
 SELECT * FROM tbl_user ;
 
 .quit
