ac367cd105c252bc6264d112788b53c5ac36a0e9
Athenais Vaginay <vaginay.athenais@gmail.com>
2017-01-25 15:49:06 +0100

    Configures upload dir

--- config.py
+++ config.py
@@ -3,6 +3,11 @@ class Config(object):
     TESTING = False
     SQLALCHEMY_DATABASE_URI = 'sqlite:///test.db' #'sqlite://:memory:'
     #engine://user:password@host:port/database
+    
+    TOP_LEVEL_DIR = '.'
+    # Uploads
+    UPLOADS_DEFAULT_DEST = TOP_LEVEL_DIR + '/default_up'
+    UPLOADED_PHOTOS_DEST = TOP_LEVEL_DIR + '/up'
 
 class ProductionConfig(Config):
     pass
