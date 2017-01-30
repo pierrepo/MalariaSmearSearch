f0e569f4c0b967eacd7c5a1293d98f6405de406d
Athenais Vaginay <vaginay.athenais@gmail.com>
2017-01-30 17:58:18 +0100

    Change var names

--- test_img_cut.py
+++ test_img_cut.py
@@ -2,30 +2,29 @@ from PIL import Image
 import math
 import itertools
 
-def make_chunks(image_path, num_h_crop = 2, num_v_crop = 2):
+def make_chunks(image_path, num_crop_col = 2, num_crop_row = 2):
     """slice an image into parts """
     img = Image.open(image_path)
     width, height = img.size
 
     # compute crop properties using image measure and the wanted number of pieces
-    h_crop_width = width / num_h_crop
-    v_crop_width = height / num_v_crop
+    width_crop_col = width / num_crop_col
+    width_crop_row = height / num_crop_row
 
     # x and y represent Cartesian pixel coordinates.
     # 0,0 is up left
-    # the norm between 2 ticks on horizontal x axis is h_crop_width
-    # the norm between 2 ticks on vertical y axis is v_crop_width
-    x = [h_crop_width * e for e in range (num_h_crop +1)]
-    y = [v_crop_width * e for e in range (num_v_crop +1)]
+    # the norm between 2 ticks on horizontal x axis is width_crop_col
+    # the norm between 2 ticks on vertical y axis is width_crop_row
+    cut_col = [width_crop_col * e for e in range (num_crop_col +1)]
+    cut_row = [width_crop_row * e for e in range (num_crop_row +1)]
     # +1 in order to have coord of rigth limit of the image
 
-    chunks_starting_coords = itertools.product(x[:-1], y[:-1])
-    chunks_ending_coords = itertools.product(x[1:], y[1:])
+    chunks_starting_coords = itertools.product(cut_col[:-1],  cut_row[:-1])
+    chunks_ending_coords = itertools.product(cut_col[1:], cut_row[1:])
 
     chunks_coords = zip (chunks_starting_coords, chunks_ending_coords)
     for chunk_idx, chunk_coords in enumerate(chunks_coords) :
-        ((left, upper), (right, lower)) = chunk_coords
-        box = (left , upper , right , lower) # pixel coords of the chunk
+        box = list(itertools.chain.from_iterable(chunk_coords)) #(left , upper , right , lower) # pixel coords of the chunk
         print (chunk_idx, box)
         new_chunk = img.crop(box)
         new_chunk.save("out_{0}.jpg".format(chunk_idx) )
