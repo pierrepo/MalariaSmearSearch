5a6eeaa69545d853a4189df6c7c11b6a43a5a4ee
Athenais Vaginay <vaginay.athenais@gmail.com>
2017-02-03 14:06:45 +0100

    Del annotation item /!\ no db connection for now

--- static/js/annotation.js
+++ static/js/annotation.js
@@ -21,7 +21,32 @@ $(document).ready(function(){
     */
 
     $('#annotations-list li').on('click', '.glyphicon-trash', function(){
-        console.log("deleeeete");
+        /*
+        The delete action has a little extra insurance against accidentally
+        fat-fingerings. It requires two clicks to actually delete something.
+        Not a nasty "ARE YOU SURE" modal popup dialog box, we're a little more
+        sly than that :
+        As you click the X, a little notice will pop out to the right
+        asking about sureness. If they click again then deleting may commence.
+        */
+        var thiscache = $(this);
+        console.log(thiscache);
+        if (thiscache.data("readyToDelete") == "go for it") {
+            console.log("deleeeete");
+            $.ajax({
+                // TODO the user wants to delete this list item, commence deleting!
+                // if success : reorder the list
+                success: function(r){
+                    thiscache
+                        .parent()
+                            .hide(400, function(){$(this).remove()});
+                }
+            });
+        }
+        else {
+            thiscache.text(" sure ?")
+            .data("readyToDelete", "go for it");
+        }
     });
 
     $('#annotations-list li').on('click', '.glyphicon-pencil', function(){
