e30aba406a505333ef17deb11fddad5c0d5fc472
Athenais Vaginay <vaginay.athenais@gmail.com>
2017-02-03 13:40:41 +0100

    Link li item buttons to events

--- static/js/annotation.js
+++ static/js/annotation.js
@@ -11,4 +11,20 @@ $(document).ready(function(){
         .append("<button class='glyphicon glyphicon-trash'></button><button class='glyphicon glyphicon-pencil'></button>");
 
 
+    /* link li item buttons to events using the on() function
+    because :
+    - 1) it only creates one event handler which is more efficient
+        than using the click function that creates a unique event handler for
+        every single list item on the page, each one taking up browser memory
+    - and 2) new items appended to the page are automatically bound by
+        the same handler. Killer.
+    */
+
+    $('#annotations-list li').on('click', '.glyphicon-trash', function(){
+        console.log("deleeeete");
+    });
+
+    $('#annotations-list li').on('click', '.glyphicon-pencil', function(){
+        console.log("edit");
+    });
 });
