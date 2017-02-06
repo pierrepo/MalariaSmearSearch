ecb90c29b9b2b4fcc63a0e5545c72c6b56817c0a
Athenais Vaginay <vaginay.athenais@gmail.com>
2017-02-06 17:39:45 +0100

    jQuerise annotation list generation

--- static/js/annotation.js
+++ static/js/annotation.js
@@ -6,4 +6,10 @@ jQuery(document).ready(function($) {
     // You can use the locally-scoped $ in here as an alias to jQuery.
 
     console.log("bim");
+
+    // wrap list item text in a span, and appply functionality buttons
+    $("#annotations-list li")
+        .wrapInner("<span>")
+        .append("<button class='glyphicon glyphicon-trash'></button><button class='glyphicon glyphicon-pencil'></button>");
+
 });
