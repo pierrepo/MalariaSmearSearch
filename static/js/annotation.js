727e16331081afe778a8772aca939d1a0fb8bf5e
Athenais Vaginay <vaginay.athenais@gmail.com>
2017-02-06 17:26:53 +0100

    Work with external js file script

---
+++ static/js/annotation.js
@@ -0,0 +1,9 @@
+console.log("pouet");
+
+jQuery.noConflict();
+
+jQuery(document).ready(function($) {
+    // You can use the locally-scoped $ in here as an alias to jQuery.
+
+    console.log("bim");
+});
