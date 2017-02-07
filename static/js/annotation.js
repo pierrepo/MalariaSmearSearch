efa9a35cc101c362d2fe89c012e4a9849e0b531c
Athenais Vaginay <vaginay.athenais@gmail.com>
2017-02-07 10:03:18 +0100

    Fail : try to add Annotorious annotation

--- static/js/annotation.js
+++ static/js/annotation.js
@@ -7,6 +7,31 @@ jQuery(document).ready(function($) {
 
     console.log("bim");
 
+    // Place annotation from db using AJAX request :
+    // TODO
+
+    the_text = 'My annotation'
+    var myAnnotation = {
+        /** The URL of the image where the annotation should go **/
+        src : $('#chunk').attr('src'),
+        /** The annotation text **/
+        text : the_text,
+        /** -> no delete icon in the annotation popup **/
+        editable: false,
+        /** The annotation shape **/
+        shapes : [{
+            /** The shape type -- rectangle is the only supported shape type **/
+            type : 'rect',
+            /** 'units' only required for pixel coordinates **/
+            units: 'pixel',
+            /** The shape geometry (pixel coordinates) **/
+            geometry : { x : 10, y: 10, width : 400, height: 600 }
+        }]
+    }
+    anno.addAnnotation(myAnnotation);
+
+
+
     // wrap list item text in a span, and appply functionality buttons
     $("#annotations-list li")
         .wrapInner("<span>")
