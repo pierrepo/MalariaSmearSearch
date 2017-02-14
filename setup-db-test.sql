71934046b611aae28530222760a11f6f72924119
Athenais Vaginay <vaginay.athenais@gmail.com>
2017-02-14 18:47:03 +0100

    Add annotation model

--- setup-db-test.sql
+++ setup-db-test.sql
@@ -44,6 +44,21 @@ CREATE TABLE tbl_chunk (
   PRIMARY KEY ('id_photo', 'col', 'row')
 );
 
+CREATE TABLE tbl_annotation (
+  'id' INTEGER PRIMARY KEY AUTOINCREMENT,
+  'username' REFERENCES tbl_user(username),
+  'id_photo' INTEGER,
+  'col' INTEGER,
+  'row' INTEGER,
+  'date' TEXT, /*ISO8601 strings ("YYYY-MM-DDTHH:MM:SS.SSS")*/
+  'x' INT,
+  'y' INT,
+  'width' INT,
+  'height' INT,
+  'annotation' VARCHAR(2) CHECK ('annotation' IN ('P' , 'RC', 'WC', 'O') ), /* parasite, red cell, white cell, other  */
+  FOREIGN KEY('id_photo', 'col', 'row') REFERENCES tbl_chunk('id_photo', 'col', 'row')
+);
+
 
 SELECT * FROM tbl_user ;
 
