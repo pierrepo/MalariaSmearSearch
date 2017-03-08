0589c90c1b472b95861fcd1764ee751e2836450f
Athenais Vaginay <vaginay.athenais@gmail.com>
2017-03-08 17:04:33 +0100

    Dummy user (with full access)

--- setup-db.py
+++ setup-db.py
@@ -1,2 +1,13 @@
 from app import db
+from model import User
+
 db.create_all()
+
+admin = User()
+admin.username = 'admin'
+admin.password = 'admin123'
+admin.level = 'admin'
+admin.institution = 'up7d'
+
+db.session.add(admin)
+db.session.commit()
