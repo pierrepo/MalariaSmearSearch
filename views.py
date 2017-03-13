3ed9d4fbee97cfe0ed8fcf42d97dbd62503c4a35
Athenais Vaginay <vaginay.athenais@gmail.com>
2017-03-13 15:16:42 +0100

    Increase the max chunk size

--- views.py
+++ views.py
@@ -88,12 +88,12 @@ def add_sample():
             )
 
             # get the number of pieces using integer division :
-            # chunk dimensions are always BELOW 1000 px
+            # chunk dimensions are always BELOW 2000 px
             with Image.open(new_sample.path) as img :
                 width, height = img.size
                 print (width, height)
-                new_sample.num_col = (width // 1000) + 1
-                new_sample.num_row = (height // 1000) + 1
+                new_sample.num_col = (width // 2000) + 1
+                new_sample.num_row = (height // 2000) + 1
             new_sample.init_on_load()
             db.session.commit()
 
