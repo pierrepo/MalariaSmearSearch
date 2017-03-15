b5ce604cb350986a06c5aebcb4024fa195342ab9
Athenais Vaginay <vaginay.athenais@gmail.com>
2017-03-15 18:50:32 +0100

    Change db setup using many to many rel + simplification
    http://docs.sqlalchemy.org/en/latest/orm/extensions/associationproxy.html#simplifying-association-objects

--- setup-db.py
+++ setup-db.py
@@ -4,11 +4,17 @@ import new_model
 db.create_all()
 db.session.commit() # create all the tables that are in model
 
+i_a = new_model.Institution ('up7d')
+i_b = new_model.Institution ('ijm')
+
 admin = new_model.User_auth()
 admin.username = 'admin'
 admin.password = 'admin123'
-admin.level = 'admin'
-admin.institution = 'up7d'
+
+for institution in (i_a, i_b) :
+    admin.institutions.append(institution)
+
+print(admin.institutions)
 
 db.session.add(admin)
 db.session.commit()
