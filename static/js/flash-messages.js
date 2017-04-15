fd3c850d51fc37aa64d20d0f19ceff08ddf9da49
Pierre Poulain <pierre.poulain@cupnet.net>
2017-04-15 13:23:37 +0200

    Better user of Bootstrap style

--- static/js/flash-messages.js
+++ static/js/flash-messages.js
@@ -6,14 +6,14 @@
 */
 var Flash = {};
 Flash.success = function(msg, time=1000){
-    $('#flash-container')[0].innerHTML = "<div class='success message'>" + msg + "</div>";
+    $('#flash-container')[0].innerHTML = "<span class='success message'>" + msg + "</span>";
     $('#flash-container').addClass('showing');
     setTimeout(function(){
       $('#flash-container').removeClass('showing');
     }, time);
   };
 Flash.error = function(msg, time=1000){
-    $('#flash-container')[0].innerHTML = "<div class='error message'>" + msg + "</div>";
+    $('#flash-container')[0].innerHTML = "<span class='error message'>" + msg + "</span>";
     $('#flash-container').addClass('showing');
     setTimeout(function(){
         $('#flash-container').removeClass('showing');
