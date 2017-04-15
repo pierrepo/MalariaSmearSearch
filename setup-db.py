15fd93b1100df4a0052cf3a4b00c657950a8579e
Pierre Poulain <pierre.poulain@cupnet.net>
2017-04-15 21:09:47 +0200

    Add real insitutions

--- setup-db.py
+++ setup-db.py
@@ -4,17 +4,16 @@ import model
 db.create_all()
 db.session.commit() # create all the tables that are in model
 
-i_original = model.Institution ('original_institution',  'place o', 'description o', 'url o', 'comment o')
-i_a = model.Institution ('up7d',  'place a', 'description a', 'url a', 'comment a')
-i_b = model.Institution ('ijm',  'place b', 'description b', 'url b', 'comment b')
-
+i_up7d = model.Institution ('up7d',  'Paris, France', 'Université Paris Diderot', 'https://www.univ-paris-diderot.fr/', '--')
+i_ijm = model.Institution ('IJM',  'Paris, France', 'Institut Jacques Monod', 'http://www.ijm.fr/', '--')
+i_fcrm = model.Institution ('FCRM',  'Brazzaville, République du Congo', 'Fondation Congolaise pour la Recherche Médicale ', 'http://www.fcrm-congo.com/', '--')
 
 admin = model.User_auth()
-admin.username = 'admin'
-admin.password = 'admin123'
-admin.primary_institution_name = i_original.name
+admin.username = 'pierre'
+admin.password = 'pierre123'
+admin.primary_institution_name = i_up7d.name
 
-for secondary_institution in (i_a, i_b) :
+for secondary_institution in (i_ijm, i_fcrm) :
     new_membership = model.Membership()
     new_membership.secondary_institution_name = secondary_institution.name
     admin.secondary_institutions.append(new_membership)
@@ -28,7 +27,7 @@ db.session.commit()
 # put the User in data.db :
 dat_user = model.User()
 dat_user.username = admin.username
-dat_user.primary_institution = i_original
+dat_user.primary_institution = i_up7d
 
 db.session.add(dat_user)
 db.session.commit()
