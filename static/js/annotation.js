004a94231f26949f85d1fb8876f4d9ba335ed0cb
Athenais Vaginay <vaginay.athenais@gmail.com>
2017-05-17 12:09:25 +0200

    Fn to shuffle annotation array is now a method of DatArray objects

--- static/js/annotation.js
+++ static/js/annotation.js
@@ -144,6 +144,23 @@ class DatArray {
             this.data.push(anno);
         }
     }
+
+
+    /* Shuffle array
+    * Fisher-Yates-Durstenfeld shuffle
+    * http://stackoverflow.com/a/3718452
+    */
+    shuffle() {
+        var sourceArray=this.data
+        for (var i = 0; i < sourceArray.length - 1; i++) {
+            var j = i + Math.floor(Math.random() * (sourceArray.length - i));
+
+            var temp = sourceArray[j];
+            sourceArray[j] = sourceArray[i];
+            sourceArray[i] = temp;
+        }
+        return sourceArray;
+    }
 }
 
 
