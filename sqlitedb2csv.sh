b3fdb4e747c6146bbb1ec37e0ed3f084c3303e92
Athenais Vaginay <vaginay.athenais@gmail.com>
2017-06-30 18:35:37 +0200

    Convert sqlite data db to flat csv containing useful columns

---
+++ sqlitedb2csv.sh
@@ -0,0 +1,47 @@
+#!/bin/bash
+now=$(date +"%Y%m%d")
+#-----
+# get space separated string of tablenames
+outputtablenames=$(sqlite3 data.db '.tables')
+echo $outputtablenames
+
+#-----
+# transform space separated string of tablenames into array of tablenames :
+IFS=' ' read -ra tablenames <<< "$outputtablenames"
+# read option :
+# r :
+#   raw input - disables interpretion of backslash escapes and line-continuation in the read data
+#   example :
+#   $ str="a\bc"; read x <<< "$str"; read -r y <<< "$str"; echo "$x"; echo "$y"
+# a :
+#   read the data word-wise into the specified array <ARRAY> = tablenames
+#   -> tablenames is an array
+
+#-----
+# iterate through tablenames and dump a csv :
+for tablename in "${tablenames[@]}"; do
+    echo $tablename ;
+    sqlite3 -header -csv data.db "select * from $tablename;" > $tablename.csv
+done
+
+#-----
+# merge these csv into a flat file :
+# containing :
+# Annotaition : x, y width, height , annotation, sample id, row, col
+# Sample :  smear type , licence, magnification,
+
+DELIMITER=","
+
+# for each line of Annotation.csv file :
+# cut -> selected input is written in the same order that it is read
+# use awk instead if wanna rearrange column order
+while read line; do
+    x_y_width_height_tag_sampleid_row_col=$(echo $line  |  cut -d "${DELIMITER}" -f 6,7,8,9,10,13,2,3);
+    sampleid=$(echo $line  |  cut -d "${DELIMITER}" -f 13);
+    smeartype_licence_magnification=$(grep "^${sampleid}," Samples.csv | cut -f 3,7,9 -d "${DELIMITER}");
+    # /!\ grep has to find only one line !
+
+    processed_annotation=$x_y_width_height_tag_sampleid_row_col,$smeartype_licence_magnification
+    echo $processed_annotation
+    echo $processed_annotation >> "extracted_data_$now.csv"
+done <Annotations.csv
