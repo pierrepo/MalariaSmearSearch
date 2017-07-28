92679c361093860647b488c41732a3f80b7b8b45
Pierre Poulain <pierre.poulain@cupnet.net>
2017-07-28 13:16:08 +0200

    User login can now be 4 characters only

--- forms.py
+++ forms.py
@@ -73,7 +73,7 @@ class LoginForm(FlaskForm):
     """
     Form for user login.
     """
-    username = TextField('Username', validators=[Required(), Length(min=5, max=80)])
+    username = TextField('Username', validators=[Required(), Length(min=4, max=80)])
     password = PasswordField('Password', validators=[Required()])
     idle_ttl = RadioField('Idle Session Timeout', default='tmp', choices=[
             ('tmp',  '20 minutes'),
